ad3f19e9e7f74d9b0f8273a4e17cec192a73690e
andreyosintsev <kintaro_oe@inbox.ru>
2025-08-19 17:30:37 +0400

    feat: block section-foxboost added

---
+++ scripts/section-ambassador.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+    let ambassadorsInitialized = false;
+    const $ambassadors = $(".section-ambassador");
+
+    function initCarousel() {
+        if (
+            $(window).width() < 1440 &&
+            $(window).width() >= 480 &&
+            !ambassadorsInitialized
+        ) {
+            $ambassadors.addClass("owl-carousel owl-theme").owlCarousel({
+                items: 1.5,
+                loop: true,
+                nav: false,
+                dots: false,
+                autoplay: false,
+                center: false,
+                pullDrag: true,
+                responsive: {
+                    600: {
+                        items: 2.5,
+                    },
+                    834: {
+                        items: 3.5,
+                    },
+                },
+            });
+
+            ambassadorsInitialized = true;
+        }
+    }
+
+    function destroyCarousel() {
+        if ($(window).width() >= 1440 || $(window).width() < 480) {
+            if (ambassadorsInitialized) {
+                $ambassadors
+                    .trigger("destroy.owl.carousel")
+                    .removeClass("owl-carousel owl-theme");
+                ambassadorsInitialized = false;
+            }
+        }
+    }
+
+    initCarousel();
+    destroyCarousel();
+
+    $(window).resize(function () {
+        initCarousel();
+        destroyCarousel();
+    });
+});
